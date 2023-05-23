744979a7a02476619efb6aad653ecb1fb54ed5ef
ibukunoluwashina <ibukunoluwashina@gmail.com>
2023-05-23 17:56:55 +0100

    .....

--- chamber/script/script.js
+++ chamber/script/script.js
@@ -1,5 +1,15 @@
+function toggleMenu() {
+    document.getElementById("primaryNav").classList.toggle("open");
+    document.getElementById("hamburgerBtn").classList.toggle("open");
+}
+
+const x = document.getElementById('hamburgerBtn');
+x.onclick = toggleMenu;
+
 const d = new Date();
 document.getElementById("currentdate").innerHTML = d;
 
 const date = new Date(document.lastModified);
 document.getElementById("lastdate").innerHTML = date;
+
+
