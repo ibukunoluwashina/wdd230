33939b9c5c078c373f83fe30dc4b55e85915ff62
ibukunoluwashina <ibukunoluwashina@gmail.com>
2023-06-15 21:16:37 +0100

    .....

--- lesson09/scripts/prophets.js
+++ lesson09/scripts/prophets.js
@@ -1,39 +1,46 @@
 const url = 'https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json';
 
-async function getProphetData(url) {
-    const response = await fetch(url);
-    const data = await response.json();
-    //console.table(data.prophets);
-    displayProphets(data.prophets);
+(async function getProphetData() {
+  const response = await fetch(url);
+  const data = await response.json();
+  // console.table(data.prophets);
+  displayProphets(data.prophets);
+})();
 
-    const displayProphets = (prophets) => {
-        const cards = document.querySelector('div.cards'); // select the output container element
-      
-        prophets.forEach((prophet) => {
-          // Create elements to add to the div.cards element
-          let card = document.createElement('section');
-          let h2 = document.createElement('h2');
-          let portrait = document.createElement('img');
-      
-          // Build the h2 content out to show the prophet's full name - finish the template string
-          h2.textContent = `${prophet.name} ____________`;
-      
-          // Build the image portrait by setting all the relevant attribute
-          portrait.setAttribute('src', prophet.imageurl);
-          portrait.setAttribute('alt', `Portrait of ${prophet.name} ______________`);
-          portrait.setAttribute('loading', 'lazy');
-          portrait.setAttribute('width', '340');
-          portrait.setAttribute('height', '440');
-      
-          // Append the section(card) with the created elements
-          card.appendChild(h2);
-          card.appendChild(portrait);
-      
-          cards.appendChild(card);
-        }); // end of forEach loop
-      } // end of function expression
-  }
-  
-  getProphetData();
+const displayProphets = (prophets) => {
+  const cards = document.querySelector('div.cards');
 
-  
+  prophets.forEach((prophet) => {
+    let card = document.createElement('section');
+    let h2 = document.createElement('h2');
+    let birthDate = document.createElement('p');
+    let birthPlace = document.createElement('p');
+    let portrait = document.createElement('img');
+
+    h2.textContent = `${prophet.name} ${prophet.lastname} -${ordinalSuffix(prophet.order)}`;
+    portrait.setAttribute('src', prophet.imageurl);
+    portrait.setAttribute('alt', `Portrait of ${prophet.name}  ${prophet.lastname}  -${prophet.order}${ordinalSuffix(prophet.order)} Latter-day President`);
+    portrait.setAttribute('loading', 'lazy');
+    portrait.setAttribute('width', '340');
+    portrait.setAttribute('height', '440');
+
+    birthDate.textContent = `Date of Birth: ${prophet.birthdate}`;
+    birthPlace.textContent = `Place of Birth: ${prophet.birthplace}`;
+
+    card.appendChild(h2);
+    card.appendChild(birthDate);
+    card.appendChild(birthPlace);
+    card.appendChild(portrait);
+    
+
+    cards.appendChild(card);
+  });
+};
+
+// Helper function to add ordinal suffix to numbers
+function ordinalSuffix(number) {
+  const suffixes = ['th', 'st', 'nd', 'rd'];
+  const remainder = number % 100;
+  return number + (suffixes[(remainder - 20) % 10] || suffixes[remainder] ||suffixes[0]);
+}
+
