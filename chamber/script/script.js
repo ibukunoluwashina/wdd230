b96010800ac179211bd932bb3edf6916a56b071b
ibukunoluwashina <ibukunoluwashina@gmail.com>
2023-06-18 21:10:28 +0100

    ......

--- chamber/script/script.js
+++ chamber/script/script.js
@@ -47,3 +47,53 @@ const lazyLoadingObserver = new IntersectionObserver(lazyLoadingImage,{
     threshold: 0.9 
 });
 imgElements.forEach((img) => lazyLoadingObserver.observe(img));
+
+
+
+// directory page 
+const url = '/chamber/data.json'
+
+fetch(url)
+.then((result) => 
+    result.json()
+).then(data=>data.forEach((data.company.forEach(data=>displayData(data))))).
+catch((err) => {
+    
+});
+
+
+const displayData=(data)=>{
+  let personData = `<section>
+  <h2>${data.name}</h2>
+<p>${data.address}</p>
+<p>${data.phoneNumber}</p>
+<a href="${data.website}"></a>
+<p>${data.membershipLevel}</p>
+<img src="${data.imageurl}"
+ alt="${data.name} picture"
+  loading="lazy" width="340" height="440">
+</section>`
+let container = document.querySelector('section');
+container.innerHTML += personData
+}
+
+// directory gid
+
+const gridbutton = document.querySelector("#grid");
+const listbutton = document.querySelector("#list");
+const display = document.querySelector("article");
+
+// The following code could be written cleaner. How? We may have to simplfiy our HTMl and think about a default view.
+
+gridbutton.addEventListener("click", () => {
+	// example using arrow function
+	display.classList.add("grid");
+	display.classList.remove("list");
+});
+
+listbutton.addEventListener("click", showList); // example using defined function
+
+function showList() {
+	display.classList.add("list");
+	display.classList.remove("grid");
+}
--- lesson09/scripts/prophets.js
+++ lesson09/scripts/prophets.js
@@ -1,5 +1,6 @@
 const url = 'https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json';
 
+
 (async function getProphetData() {
   const response = await fetch(url);
   const data = await response.json();
